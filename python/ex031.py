2f651bc85c8e27d2aaf8625a8a28b891efe5e3a5
edilsonbim <edilsonbim@hotmail.com.br>
2025-03-25 13:00:40 -0300

    update

--- python/ex031.py
+++ python/ex031.py
@@ -3,4 +3,9 @@ if salario < 1250:
     aumento = salario * 1.15
 else:
     aumento = salario * 1.10
-print('Quem ganahava R${:.2f} passa a ganhar R${:.2f} agora.'.format(salario, aumento))
+# \033[0:30:41m para mudar a cor, e \033[m pra voltar original
+# style none = 0
+# boold = 1
+# underline = 4
+# negative = 7
+print('Quem ganahava \033[0:30:41mR${:.2f}\033[m passa a ganhar \033[0:29:42mR${:.2f}\033[m agora.'.format(salario, aumento))
