1dd101bd3c4ee622743eba33d7d12144e15e7ae4
edilsonbim <edilsonbim@hotmail.com.br>
2025-03-26 15:38:13 -0300

    update

---
+++ python/extra/aula.py
@@ -0,0 +1,11 @@
+mapa = [0,0,1,0,0,1,0,0,1,0]
+for c in range(0, 10):
+    if mapa[c] == 0:
+        print('passo')
+        print('pula')
+        input()
+    else:
+        print('pega')
+        input()
+print('passo')
+print('pega maçã')
