d4a6b546d0253244d42c99587dc000aab22d1723
edilsonbim <edilsonbim@hotmail.com.br>
2025-03-28 20:17:05 -0300

    update

--- python/modulo2/ex067.py
+++ python/modulo2/ex067.py
@@ -3,5 +3,5 @@ while True:
     if n < 0:
         break
     for c in range(1, 11):
-        print(f' {n} x {c} = \033[1:29:48m{c * n}\033[m ')
+        print(f' {n} x {c} = \033[1:32:48m{c * n}\033[m ')
 print('Programa encerrado. Volte sempre!')
--- python/modulo2/ex071.py
+++ python/modulo2/ex071.py
@@ -1,6 +1,6 @@
-print('\033[1:31m=\033[m' * 12)
-print('BANCO BIM')
-print('\033[1:31m=\033[m' * 12)
+print('\033[1:31m=\033[m' * 30)
+print(f'{'BANCO BIM':^30}')
+print('\033[1:31m=\033[m' * 30)
 saldo = int(input('Qual valor você quer sacar?: R$'))
 cinquenta = saldo // 50
 saldo -= cinquenta * 50
@@ -10,9 +10,16 @@ dez = saldo // 10
 saldo -= dez * 10
 um = saldo // 1
 saldo -= um * 1
-print('\033[1:31m=\033[m' * 12)
-print(f'Total de {cinquenta} notas de R$50')
-print(f'Total de {vinte} notas de R$20')
-print(f'Total de {dez} notas de R$10')
-print(f'Total de {um} notas de R$1')
+print('\033[1:31m=\033[m' * 30)
+if cinquenta > 0:
+    print(f'Total de {cinquenta} notas de R$50')
+if vinte > 0:
+    print(f'Total de {vinte} notas de R$20')
+if dez > 0:
+    print(f'Total de {dez} notas de R$10')
+if um > 0:
+    print(f'Total de {um} notas de R$1')
+print('\033[1:31m=\033[m' * 30)
+print(f'{'Volte sempre ao BANCO BIM! um bom dia!':^30}')
+print('\033[1:31m=\033[m' * 30)
 #print(saldo)
