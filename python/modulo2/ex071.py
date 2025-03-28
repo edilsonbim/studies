1b552431a63023493e84c7013dcbfa462e461853
edilsonbim <edilsonbim@hotmail.com.br>
2025-03-28 17:33:19 -0300

    update

--- python/modulo2/ex071.py
+++ python/modulo2/ex071.py
@@ -2,17 +2,17 @@ print('\033[1:31m=\033[m' * 12)
 print('BANCO BIM')
 print('\033[1:31m=\033[m' * 12)
 saldo = int(input('Qual valor você quer sacar?: R$'))
-c = saldo // 50
-saldo -= c * 50
-v = saldo // 20
-saldo -= v * 20
-d = saldo // 10
-saldo -= d * 10
-u = saldo // 1
-saldo -= u * 1
+cinquenta = saldo // 50
+saldo -= cinquenta * 50
+vinte = saldo // 20
+saldo -= vinte * 20
+dez = saldo // 10
+saldo -= dez * 10
+um = saldo // 1
+saldo -= um * 1
 print('\033[1:31m=\033[m' * 12)
-print(f'Total de {c} notas de R$50')
-print(f'Total de {v} notas de R$20')
-print(f'Total de {d} notas de R$10')
-print(f'Total de {u} notas de R$1')
+print(f'Total de {cinquenta} notas de R$50')
+print(f'Total de {vinte} notas de R$20')
+print(f'Total de {dez} notas de R$10')
+print(f'Total de {um} notas de R$1')
 #print(saldo)
