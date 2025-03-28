561d1247afd9296b480c2d870bd392e5e17092f2
edilsonbim <edilsonbim@hotmail.com.br>
2025-03-28 11:35:12 -0300

    update

--- python/modulo2/ex065.py
+++ python/modulo2/ex065.py
@@ -3,6 +3,6 @@ lista = []
 while x == 'S':
     n = float(input('Digite um numero: '))
     lista = lista + [n]
-    x = str(input('Quer continuar? [S/N]: ')).upper().strip()
+    x = str(input('Quer continuar? [S/N]: ')).upper().strip()[0]
 print(lista)
 print('A media dos {} numeros digitados é {:.2f}'.format(len(lista), sum(lista) / len(lista)))
