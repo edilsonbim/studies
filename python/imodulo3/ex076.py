1be6a33183c0182c84758c5be9e4cbe341fe528b
edilsonbim <edilsonbim@hotmail.com.br>
2025-07-04 14:07:22 -0300

    update

--- python/imodulo3/ex076.py
+++
@@ -1,9 +0,0 @@
-lista = [int(input('Digite um número: '))]
-lista = lista + [int(input('Digite outro número: '))]
-lista = lista + [int(input('Digite mais um número: '))]
-lista = lista + [int(input('Digite opo último número: '))]
-print(f'Você digitou os valores {lista}')
-c = 0
-
-print(f'O valor 9 apareceu {9 in lista:.0f} vezes.')
-print(c)
